6213249eb1ce7b2c831a7706e6a71e36a24b673d
Aida Suraya Binti Abd Aiz <aida059@Aidas-MacBook-Air-2.local>
2025-06-26 12:27:52 +0800

    Make a whole page

--- MATA/script.js
+++ MATA/script.js
@@ -1,10 +1,15 @@
+const triggerShape = document.querySelector('.trigger-shape');
 const wrapper = document.querySelector('.wrapper');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
-const btnPopup = document.querySelector('.btnlogin-popup');
+// const btnPopup = document.querySelector('.btnlogin-popup');
 const iconClose = document.querySelector('.icon-close');
 const mobileLoginLink = document.querySelector('.mobile-login-link');
 const mobileRegisterLink = document.querySelector('.mobile-register-link');
+
+triggerShape.addEventListener('click', () => {
+    wrapper.classList.add('active-popup');
+});
 
 registerLink.addEventListener('click', ()=> {
     wrapper.classList.add('active');
@@ -14,9 +19,9 @@ loginLink.addEventListener('click', ()=> {
     wrapper.classList.remove('active');
 });
 
-btnPopup.addEventListener('click', ()=> {
+/* btnPopup.addEventListener('click', ()=> {
     wrapper.classList.add('active-popup');
-});
+}); */
 
 iconClose.addEventListener('click', ()=> {
     wrapper.classList.remove('active-popup');
